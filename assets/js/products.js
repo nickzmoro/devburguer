2fce728ca992a9f5ebda8a5182cb83e58a08f13e
nickzmoro <nicolasmororuiz@gmail.com>
2025-01-13 00:05:44 -0300

    // melhoria na organização das pastas

---
+++ assets/js/products.js
@@ -0,0 +1,8 @@
+const menuOptions = [
+    { name: 'X-Salada', price: 30, vegan: false, discount: true, src: 'assets/img/xsalada.jpeg' },
+    { name: 'X-Bacon', price: 34, vegan: false, discount: false, src: 'assets//img/xbacon.png' },
+    { name: 'X-Bacon Egg', price: 39, vegan: false, discount: true, src: 'assets//img/bacon-egg.png' },
+    { name: 'Monstruoso', price: 50, vegan: false, discount: false, src: 'assets//img/monstruoso.png' },
+    { name: 'Big Vegano', price: 55, vegan: true, discount: true, src: 'assets//img/xvegan.png' },
+    { name: 'X-Vegan', price: 45, vegan: true, discount: false, src: 'assets//img/monstruoso-vegan.png' },
+]
